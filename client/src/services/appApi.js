82584708126165e2c708b268dd2f925bb1097457
yoni3593 <yoni263593@gmail.com>
2023-01-29 16:21:23 +0200

    cheng API server

--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3005" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3006" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
